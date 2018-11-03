76b730c9022aea962b4ab8ee0644be0a32cc1599
Bruno Carlin <brunotestaguzza@gmail.com>
2018-11-03 16:07:52 -0300

    Fixed some bugs, uploading a new commodity function

--- R/bonsai_cv_models.R
+++ R/bonsai_cv_models.R
@@ -1,3 +1,18 @@
+#' Uses cross validation with a list of functions
+#' 
+#' Commodity function to facilitate the usage of the bonsai.cv function, may be depriciated
+#' @param y Univariate time series
+#' @param list_functions a list of functions to test
+#' @param h Forecast horizon
+#' @param window Length of the rolling window, if NULL, a rolling window will not be used.
+#' @param Start how many observations before starting to calculate errors
+#' @param max_fold how many times should the function calculate errors
+#' @param min_lenght how many observations are nescessary before starting to calculate errors 
+#' @details Let y contain the time series y[1:T]. Then forecastfunction is applied successively to the time series y[1:t], for t=1,…,T-h, making predictions f[t+h]. The errors are given by e[t+h] = y[t+h]-f[t+h]. If h=1, these are returned as a vector, e[1:T]. For h>1, they are returned as a matrix with the hth column containing errors for forecast horizon h. The first few errors may be missing as it may not be possible to apply forecastfunction to very short time series.
+#' @return Numerical time series object containing the forecast errors as a vector (if h=1) and a matrix otherwise. The time index corresponds to the last period of the training data. The columns correspond to the forecast horizons.
+#' @return A matrix with the weighted object
+#' @author Rob Hyndman created the tsCV {forecast}, heavily based on it
+#' @export 
 bonsai_cv_models <- function(
   y,
   list_functions,
